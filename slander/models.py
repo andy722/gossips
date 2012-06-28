ffdb582668ac537fda84835bb94d938a24fa610e
andy <andy@abelsky.com>
2012-06-28 21:55:47 +0700

    Initial import.

---
+++ slander/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class PersonProfile(models.Model):
+    pass
+
+
+class VKProfile(models.Model):
+    pass
+
+
+class Tale(models.Model):
+    pass
